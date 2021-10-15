4b8f55291eb446521a34b41da93f2112487a7719
leanrobert <53313456+leanrobert@users.noreply.github.com>
2021-10-15 11:54:32 -0300

    part 8.5

--- phonebook/library-backend.js
+++ phonebook/library-backend.js
@@ -107,7 +107,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(genre: String, author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -116,7 +116,22 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (root, args) => books.filter(book => book.author === args.author),
+    allBooks: (root, args) => {
+      let filterBooks
+      if(!args.author && !args.genre) {
+        return books
+      }
+
+      if(!args.author) {
+        return books.filter(book => book.genres.includes(args.genre))
+      }
+
+      if(!args.genre) {
+        return books.filter(book => book.author === args.author)
+      }
+
+      return books.filter(book => book.genres.includes(args.genre)).filter(book => book.author === args.author)
+    },
     allAuthors: () => authors
   },
 
